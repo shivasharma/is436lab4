1a651c1e5bd21fc6f93e26dfdee1581800bd4b65
Shiva Sharma <ss12@umbc.edu>
2023-11-06 12:45:52 -0500

    Added more code

--- diagram.py
+++ diagram.py
@@ -2,10 +2,13 @@ from diagrams import Diagram
 from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
+#from diagrams.onprem.client import Client
+from diagrams.programming.framework import *
+from diagrams.programming.language import *
+from diagrams.generic.database import *
+
 
 with Diagram("Grouped Workers", show=False, direction="TB"):
-    ELB("lb") >> [EC2("worker1"),
-                  EC2("worker2"),
-                  EC2("worker3"),
-                  EC2("worker4"),
-                  EC2("worker5")] >> RDS("events")
+ 
+    Angular("Angular") >> Java("java") >>Spring("Spring")
+    PHP("Backend") >> SQL("DB")
---
+++ second.py
@@ -0,0 +1,27 @@
+from diagrams import Cluster, Diagram, Node, Edge
+from diagrams.k8s.compute import Pod
+from diagrams.k8s.network import Ing
+from diagrams.gcp.network import LoadBalancing
+from diagrams.onprem.network import Nginx
+from diagrams.onprem.client import Client
+
+with Diagram("IS436 APplication", show=False):
+
+    gcp_lb = Client("User")
+
+    with Cluster("Kubernetes"):
+
+        with Cluster("Nginx"):
+            nginx= Nginx("")
+
+        with Cluster("MyApp"):
+            myapp_ing = Ing("")
+            with Cluster("Pods"):
+                myapp_pods = Pod("myapp")
+
+        with Cluster("MySQL"):
+            myapp_db = Pod("myapp-db")
+
+    gcp_lb >> Edge(headport="c", tailport="c", minlen="1", lhead='cluster_Kubernetes') >> nginx
+    nginx >> Edge(headport="c", tailport="c", minlen="1", lhead='cluster_MyApp') >> myapp_ing >> Edge(headport="c", tailport="c", minlen="1", lhead='cluster_MyApp pods') >> myapp_pods >> myapp_db
+
