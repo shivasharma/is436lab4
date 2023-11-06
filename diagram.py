a2da1d21684c084adb1688878f9dbd39fe1f68d4
Shiva Sharma <ss12@umbc.edu>
2023-11-06 13:29:47 -0500

    More code

--- diagram.py
+++ diagram.py
@@ -1,4 +1,4 @@
-from diagrams import Diagram
+from diagrams import Diagram,Cluster
 from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
@@ -8,9 +8,17 @@ from diagrams.programming.language import *
 from diagrams.generic.database import *
 from diagrams.onprem.client import Users
 from diagrams.onprem.database import PostgreSQL
+from diagrams.onprem.database import *
 from diagrams.onprem.ci import Jenkins
+
 with Diagram("IS 436 ", show=False, direction="TB"):
-    Users("client")
+    client=Users("client")
     Angular("Angular") >> Java("java") >> Spring("Spring")
-    PHP("Backend") >> SQL("DB") >> PostgreSQL("PGaDmin")
-    Jenkins("Ci/CD")
+   
+
+    with Cluster("Group component"):
+        workers=Jenkins("Jenkins") 
+    with Cluster("Group Cluster"):
+        source= PHP("Backend") >> SQL("DB") >> PostgreSQL("PGaDmin")
+
+    client >> source
